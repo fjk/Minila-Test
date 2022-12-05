e9140fc1eb0e5e4ccdbd0acaa23dba47917b649d
fjk <frank.kropp@hotmail.com>
2022-12-05 23:42:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Minila::Test - It's new $module
+
+# SYNOPSIS
+
+    use Minila::Test;
+
+# DESCRIPTION
+
+Minila::Test is ...
+
+# LICENSE
+
+Copyright (C) fjk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+fjk <frank.kropp@hotmail.com>
---
+++ lib/Minila/Test.pm
@@ -0,0 +1,39 @@
+package Minila::Test;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Minila::Test - It's new $module
+
+=head1 SYNOPSIS
+
+    use Minila::Test;
+
+=head1 DESCRIPTION
+
+Minila::Test is ...
+
+=head1 LICENSE
+
+Copyright (C) fjk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+fjk E<lt>frank.kropp@hotmail.comE<gt>
+
+=cut
+
